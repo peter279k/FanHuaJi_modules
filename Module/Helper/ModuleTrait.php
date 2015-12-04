7c76f3594b7be7da6db590fb7acd7ae4dd84fb9e
2D-Gate <admin@2d-gate.org>
2015-12-05 03:46:49 +0800

    Add getRandomWeightedElement() to the module trait

--- Module/Helper/ModuleTrait.php
+++ Module/Helper/ModuleTrait.php
@@ -66,4 +66,25 @@ trait ModuleTrait {
         });
     }
 
+    /**
+     * Utility function for getting random values with weighting.
+     * Pass in an associative array, such as array('A'=>5, 'B'=>45, 'C'=>50)
+     * An array like this means that "A" has a 5% chance of being selected, "B" 45%, and "C" 50%.
+     * The return value is the array key, A, B, or C in this case.  Note that the values assigned
+     * do not have to be percentages. The values are simply relative to each other. If one value
+     * weight was 2, and the other weight of 1, the value with the weight of 2 has about a 66%
+     * chance of being selected. Also note that weights should be integers.
+     * @param  array  $array [an array whose values are positive integer weightings]
+     * @return               [a randomly picked array key]
+     */
+    private function getRandomWeightedElement (array $array) {
+        $rand = mt_rand(1, (int) array_sum($array));
+        foreach ($array as $key => &$weighting) {
+            $rand -= $weighting;
+            if ($rand <= 0) {
+                return $key;
+            }
+        }
+    }
+
 }
